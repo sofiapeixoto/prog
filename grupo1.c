261cc46abb9d697b9d66ae23d1f5360136a8fd63
sofiapeixoto <sofiapeixoto54@gmail.com>
2024-06-14 13:55:36 +0100

    criar file

---
+++ grupo1.c
@@ -0,0 +1,5 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file.c to edit this template
+ */
+
