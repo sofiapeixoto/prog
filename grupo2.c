d04070e36eb62f55947b0f71ffed8fada477ef97
sofiapeixoto <sofiapeixoto54@gmail.com>
2024-06-14 13:58:33 +0100

    teste

--- grupo2.c
+++
@@ -1,5 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file.c to edit this template
- */
-
