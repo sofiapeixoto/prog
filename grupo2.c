1cab87bc0563d879e59b0ab389383b4dafd4f818
sofiapeixoto <sofiapeixoto54@gmail.com>
2024-06-14 14:00:51 +0100

    teste2

---
+++ grupo2.c
@@ -0,0 +1,6 @@
+printf("1º pergunta: %s\n", str[j]= pergunta);
+    printf("1Resposta 1: %s\n", str[j]= r1);
+    printf("Resposta 2: %s\n", str[j]= r2);
+    printf("Resposta 3: %s\n", str[j]= r3);
+    printf("Resposta certa: %s", str[j]= rcerta);
+    
