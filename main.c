4637751a283497a5a6dde8c7a435c4a91b96eb8c
sofiapeixoto <sofiapeixoto54@gmail.com>
2024-06-14 17:56:20 +0100

    Other modifications

--- main.c
+++ main.c
@@ -8,27 +8,36 @@
 
 
 int main() {
-    
+    int numperguntas, pontos, potuacao=0, op;
     char nome[20], sigla[10];
-    printf("Bem-Vindo ao jogo de EMECA!!!\n");
-
-    printf("Insira o seu nome: ");
-    fgets(nome, sizeof(nome), stdin);
-    nome[strcspn(nome, "\n")] = 0;
-
-    printf("Insira a sua sigla: ");
-    fgets(sigla, sizeof(sigla), stdin);
-    sigla[strcspn(sigla, "\n")] = 0;
-
     
-    printf("\nBem-Vindo, %s (%s)!\n", nome, sigla);
-    printf("1- Jogo Curto\n");
-    printf("2- Jogo Normal\n");
-    printf("0- SAIR\n\n");
-    printf("INSIRA OPÇÃO: ");
+    printf("Bem-Vindo ao jogo de EMECA!!!\n");
+    
+    entraradmin();
+    
+    printf ("\nBem-Vindo, %s (%s)!\n", nome, sigla);
+    printf ("1- Jogo Curto\n");
+    printf ("2- Jogo Normal\n");
+    printf ("0- SAIR\n\n");
+    printf ("INSIRA OPÇÃO: ");
     scanf("%d", &op);
     
+    switch (op) {
+        case 1:
+            numperguntas = 2;
+            break;
+        case 2:
+            numperguntas = 4;
+            break;
+        case 0:
+            printf("A sair do jogo...\n");
+            return 0;
+            
+        default:
+            printf("Opção inválida!!\n");
+            return 0;
+    }
     
-    printf("ORDENAR!! Colocar por ordem as 3 opções apresentadas. Todas as perguntas têm a mesma cotação.\n");
-    ordemFile();
+    
+    
 }
