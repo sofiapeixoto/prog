612c6d957ae856cbeccc658b2cf3876175f629e5
sofiapeixoto <sofiapeixoto54@gmail.com>
2024-06-15 15:26:44 +0100

    ajeitei os erros

--- grupo1.h
+++ grupo1.h
@@ -7,8 +7,17 @@
 #include <time.h>
 #include <string.h>
 
+typedef struct {
+    char pergunta[300];
+    char r1[100];
+    char r2[100];
+    char r3[100];
+    char r4[100];
+    int rcerta;
+} Perguntas1;
 
 int lerPerguntas1(Perguntas1 perguntas1[]);
 void baralharPerguntas1(Perguntas1 perguntas1[], int totalperguntas);
 void meterPerguntas1();
-#endif 
+
+#endif // GRUPO1_H
