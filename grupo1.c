3097f7ce2d3735c9432d00401a60e4c602c4673e
ip-123 <inespereira1400@gmail.com>
2024-06-15 12:21:43 +0100

    adicionei baralhar perguntas

--- grupo1.c
+++ grupo1.c
@@ -1,36 +1,39 @@
 #include "grupo1.h"
 
 void grupo_1_admin(){
-    char pergunta[100];
+typedef struct{
+    char pergunta[20];
     char r1[50];
     char r2[50];
     char r3[50];
     char r4[50];
     int rcerta;
-    char linha[200];
-    int  i=0;
-    char *str[10];
-    
-    
+}Perguntas1;
+
+Perguntas1 perguntas[30];
+
+
     FILE *ficheiro;
-  
+    char linha[300];
+    char str[30];
+    int i=0;
     
     
-    printf("Pergunta: "); gets(pergunta);
-    printf("Resposta 1: "); gets(r1);
-    printf("Resposta 2: "); gets(r2);
-    printf("Resposta 3: "); gets(r3);
-    printf("Resposta 4: "); gets(r4);
-    printf("Resposta certa: ");scanf("%d",&rcerta);
-        
+    printf("Pergunta: "); gets(perguntas[i].pergunta);
+    printf("Resposta 1: "); gets (perguntas[i].r1);
+    printf("Resposta 2: "); gets (perguntas[i].r2);
+    printf("Resposta 3: "); gets (perguntas[i].r3);
+    printf("Resposta 4: "); gets (perguntas[i].r4);
+    printf("Resposta certa: ");scanf("%d",&perguntas[i].rcerta); 
+    
     ficheiro =fopen("perguntas_grupo_1.txt", "a+");
     
-     fprintf(ficheiro, "%s;\n%s;\n%s;\n%s;\n%s;\n%d\n",pergunta,r1,r2,r3,r4,rcerta );
         
     if (ficheiro == NULL) {
         printf("Erro ao abrir o ficheiro.\n");
         
     }
+    fprintf(ficheiro, "%s;\n%s;\n%s;\n%s;\n%s;\n%d\n",perguntas[i].pergunta,perguntas[i].r1,perguntas[i].r2,perguntas[i].r3,perguntas[i].r4,perguntas[i].rcerta );
     
     while(fgets(linha, sizeof(linha), ficheiro)!=NULL){
         
@@ -44,12 +47,12 @@ void grupo_1_admin(){
             i++;
         }
         if(i==10){
-            str[0]=pergunta;
-            str[1]=r1;
-            str[2]=r2;
-            str[3]=r3;
-            str[4]=r4;
-            str[5]=rcerta; 
+            str[0]=perguntas[i].pergunta;
+            str[1]=perguntas[i].r1;
+            str[2]=perguntas[i].r2;
+            str[3]=perguntas[i].r3;
+            str[4]=perguntas[i].r4;
+            str[5]=perguntas[i].rcerta; 
             i++;
         }
     }
@@ -57,22 +60,22 @@ void grupo_1_admin(){
     
     for(int j=0; j<i; j++){
         
-    printf("1º pergunta: %s\n", str[j]=pergunta);
-    printf("1Resposta 1: %s\n", str[j]=r1);
-    printf("Resposta 2: %s\n", str[j]=r2);
-    printf("Resposta 3: %s\n", str[j]=r3);
-    printf("Resposta 4: %s\n", str[j]=r4);
-    printf("Resposta certa: %s", str[j]=rcerta);
+    printf("1º pergunta: %s\n", perguntas[i].pergunta);
+    printf("1Resposta 1: %s\n", perguntas[i].r1);
+    printf("Resposta 2: %s\n", perguntas[i].r2);
+    printf("Resposta 3: %s\n", perguntas[i].r3);
+    printf("Resposta 4: %s\n", perguntas[i].r4);
+    printf("Resposta certa: %", perguntas[i].rcerta);
     }
     
     srand(time(NULL));
-    int randomperguntas = rand()%i;
-    
-    printf("Pergunta: %s\n", pergunta[randomperguntas]=pergunta);
-    printf("1) %s\n", pergunta[randomperguntas]=r1);
-    printf("2) %s\n", pergunta[randomperguntas])=r2;
-    printf("3) %s\n", pergunta[randomperguntas])=r3;
-    printf("4) %s\n", pergunta[randomperguntas])=r4;
+    int randomPergunta = rand()% i;
+     
+        printf("Pergunta: %s\n", perguntas[randomPergunta].pergunta);
+        printf("R1: %s\n", perguntas[randomPergunta].r1);
+        printf("R2: %s\n", perguntas[randomPergunta].r2);
+        printf("R3: %s\n", perguntas[randomPergunta].r3);
+        printf("R4: %s\n", perguntas[randomPergunta].r4);
     
     
 }
