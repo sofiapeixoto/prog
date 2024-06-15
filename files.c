513e93a19d9c0a1bb14088d778f41e9d10c4c366
sofiapeixoto <sofiapeixoto54@gmail.com>
2024-06-15 15:48:45 +0100

    ajeitei os erros

--- files.c
+++ files.c
@@ -82,3 +82,63 @@ void meterPerguntaOrdem() {
     fprintf(perguntasOrdem, "%s;%s;%s;%s;%d\n", pergunta.pergunta, pergunta.r1, pergunta.r2, pergunta.r3, pergunta.rcerta);
     fclose(perguntasOrdem);
 }
+
+void entraradmin() {
+    int admin, grupos;
+    char utilizador[50], password[10];
+    char utilizador1[] = "sofia";
+    char utilizador2[] = "ines";
+    char password1[] = "2006";
+
+    printf("(1) Jogador ou (2) Administrador?\n");
+    scanf("%d", &admin);
+    getchar();
+
+    switch (admin) {
+        case 1:
+            // Código para jogador
+            break;
+
+        case 2:
+            while (1) {
+                printf("Utilizador: ");
+                scanf("%s", utilizador);
+
+                printf("Password: ");
+                scanf("%s", password);
+
+                if ((strcmp(utilizador, utilizador1) == 0 || strcmp(utilizador, utilizador2) == 0) && strcmp(password, password1) == 0) {
+                    printf("Acesso autorizado!\n");
+                    printf("Qual grupo deseja aceder?\n (1) Grupo1\n (2) Grupo2\n (3) Grupo3\n");
+                    scanf("%d", &grupos);
+
+                    switch (grupos) {
+                        case 1:
+                            meterPerguntas1(); 
+                            break;
+
+                        case 2:
+                            meterPerguntas2(); 
+                            break;
+
+                        case 3:
+                            meterPerguntaOrdem();
+                            break;
+
+                        default:
+                            printf("Opção inválida!\n");
+                            break;
+                    }
+                    return;
+                } else {
+                    printf("NÃO ESTÁ AUTORIZADO!!!\n");
+                }
+            }
+            break;
+
+        default:
+            printf("Opção inválida!\n");
+            break;
+    }
+}
+
